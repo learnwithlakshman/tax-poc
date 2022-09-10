77e02e5012c730d0b998b941962ea03dd966eea8
lakshmi narayana <lakshmi.narayana@monetize360.io>
2022-09-10 23:39:56 +0530

    Removed test cases

--- src/test/java/com/lwl/taxpoc/TaxPocApplicationTests.java
+++ src/test/java/com/lwl/taxpoc/TaxPocApplicationTests.java
@@ -1,12 +1,12 @@
 package com.lwl.taxpoc;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TaxPocApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
